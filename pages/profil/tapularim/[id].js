86346bf0f53d9157eb761e900bdd20b66fb950c0
Berkay <67913214+berkayermis@users.noreply.github.com>
2022-11-12 17:52:03 +0300

    ADD Page Structures

--- pages/profil/tapularim/[id].js
+++ pages/profil/tapularim/[id].js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="max-w-lg mt-36 mx-auto text-center px-4">
       <Head>
-      <title>Giris - e-devlet</title>
+      <title>Tapu - E-Devlet</title>
         <meta name="description" content="Interact with a simple smart contract from the client-side." />
         <link rel="icon" href="/favicon.ico" />
       </Head>

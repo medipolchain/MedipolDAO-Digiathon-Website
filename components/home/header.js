0a2d65d73217b891615e388a59bda6f0408a75e2
Berkay <67913214+berkayermis@users.noreply.github.com>
2022-11-13 00:55:23 +0300

    Update header

--- components/home/header.js
+++ components/home/header.js
@@ -3,17 +3,27 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { axiosClient } from "../../utils/axiosClient";
 import styles from "./styles.module.css";
+import { useAccount } from "../web3/hooks";
+import { getCookie } from "cookies-next";
 
 export default function LoginPage() {
+  const { account } = useAccount();
   const [user, setUser] = useState({});
 
-  const giris = async () => {
-    await axiosClient.post("/giris", {
-      user: user,
-    });
-  };
-
-  // prototype
+  useEffect(() => {
+    if (account) {
+      axiosClient
+        .post(`verify`, {
+          token: getCookie('jwt')
+        }).then((res) => {
+          axiosClient.post('get_user_by_tckn', {
+            tckn: res.data.detail.user.tckn
+          }).then((res) => {
+            setUser(res.data)
+          })
+        })
+    }
+  }, [account?.data]);
 
   return (
     <div className={styles.header}>
@@ -27,15 +37,21 @@ export default function LoginPage() {
           </a>
         </Link>
         <div className="flex items-center gap-3">
-          <Link href="/giris">
-            <a className={styles.headerButton}>Connect Wallet</a>
-          </Link>
+{/*           <Link href="/giris">
+            <a onClick={connect} className={styles.headerButton}>Connect Wallet</a>
+          </Link> */}
           <Link href="/giris">
             <a className={styles.headerButton}>Hızlı Çözüm</a>
           </Link>
-          <Link href="/giris">
+          {account?.data ? (
+            <Link href="/profil">
+              <a className={styles.headerButton}>{user?.name+" "+user?.surname}</a>
+            </Link>
+          ): (
+            <Link href="/giris">
             <a className={styles.headerButtonLogin}>Giriş Yap</a>
           </Link>
+          )}
         </div>
       </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { Web3Provider } from "../components/web3/providers";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <Web3Provider>
+        <Component {...pageProps} />
+    </Web3Provider>
+  )
 }
 
 export default MyApp

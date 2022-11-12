be1f3f207286f007989aafae21fbdb47a08024d9
fetihonal <fetihonal0@gmail.com>
2022-11-12 23:41:28 +0300

    search comp

--- components/home/heroBlock.js
+++ components/home/heroBlock.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
 import { axiosClient } from "../../utils/axiosClient";
 import styles from "./styles.module.css";
 
 export default function HeroBlock() {
   const [user, setUser] = useState({});
+  const [activeSearch, setActiveSearch] = useState(false);
 
   const giris = async () => {
     await axiosClient.post("/giris", {
@@ -21,6 +23,15 @@ export default function HeroBlock() {
           <input
             placeholder="Merhaba, size nasıl yardım edebilirim?"
             className={styles.searchInput}
+            style={{
+              borderBottomLeftRadius: activeSearch ? "0" : "",
+              borderBottomRightRadius: activeSearch ? "0" : "",
+            }}
+            onChange={(e) =>
+              e.target.value.length >= 3
+                ? setActiveSearch(true)
+                : setActiveSearch(false)
+            }
           />
           <button className="absolute right-2 top-2">
             <img
@@ -28,6 +39,13 @@ export default function HeroBlock() {
               className="max-w-xl w-11 h-11"
             />
           </button>
+          {activeSearch && (
+            <div className="bg-white rounded-b-xl p-5 absolute w-full border-t border-gray-200">
+              <Link href="">
+                <a className={styles.searchLink}>Dijital Emlak Pazaryeri</a>
+              </Link>
+            </div>
+          )}
         </div>
 
         <p className="text-sm  block text-center text-white mt-3">
